69cce16ac0fd9194b6c8b0db3a0b22f6237d12ce
jlooper <jen.looper@gmail.com>
2018-12-17 14:26:28 -0500

    missed a file

--- .vuepress/theme/components/Cards.vue
+++ .vuepress/theme/components/Cards.vue
@@ -1,15 +1,13 @@
 <template>
-
 <div class="flex flex-wrap pt-5 m-2 justify-center"> 
     <div v-for="item in $page.frontmatter.cards" class="w-1/4 m-2 cursor-pointer bg-white max-w-sm rounded shadow-lg text-center" @click="goToRoute(item.link)">
         <img class="pt-5" :src="'./images/'+item.image+''" :alt="item.title">
-        <div class="px-6 py-2">
-            
-                <div class="text-blue-darkest font-bold text-xl mb-2">{{item.title}}</div>
-                    <p class="text-blue-darker">
-                        {{item.blurb}}
-                    </p>
-                </div>  
+        <div class="px-6 py-2">  
+            <div class="text-blue-darkest font-bold text-xl mb-2">{{item.title}}</div>
+                <p class="text-blue-darker">
+                    {{item.blurb}}
+                </p>
+            </div>  
     </div>   
 </div>
 </template>
