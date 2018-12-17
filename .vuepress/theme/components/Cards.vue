104a90f8ede11163a7d4709bd841eed3caf66a11
jlooper <jen.looper@gmail.com>
2018-12-17 14:31:14 -0500

    RWD cards!

--- .vuepress/theme/components/Cards.vue
+++ .vuepress/theme/components/Cards.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="flex flex-wrap pt-5 m-2 justify-center"> 
-    <div v-for="item in $page.frontmatter.cards" class="w-1/4 m-2 cursor-pointer bg-white max-w-sm rounded shadow-lg text-center" @click="goToRoute(item.link)">
+    <div v-for="item in $page.frontmatter.cards" class="lg:w-1/4 md:w-1/2 m-2 cursor-pointer bg-white rounded shadow-lg text-center" @click="goToRoute(item.link)">
         <img class="pt-5" :src="'./images/'+item.image+''" :alt="item.title">
         <div class="px-6 py-2">  
             <div class="text-blue-darkest font-bold text-xl mb-2">{{item.title}}</div>
