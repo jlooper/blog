ed7af7ebd59cb6a70a814a6d77f232e3ba607e4c
jlooper <jen.looper@gmail.com>
2018-12-27 13:31:44 -0500

    images for blog, and finishing the list of articles

--- .vuepress/theme/layouts/BlogLayout.vue
+++ .vuepress/theme/layouts/BlogLayout.vue
@@ -2,19 +2,31 @@
     <div class="blog-body bg-white rounded p-5 m-5 text-blue-darkest">
         <h1 class="pb-5 text-blue-darkest">{{$page.frontmatter.title}}</h1>
         
-            <div class="lg:flex m-5 bg-purple-lightest rounded" v-for="article in $page.frontmatter.articles">
-                <div class="m-5">
-                    <p class="text-blue-darkest font-bold text-lg">{{article.title}}</p>
-                    <p class="text-blue-darkest text-sm">{{article.date}}</p>
-                    <p class="text-blue-darkest text-lg">{{ article.blurb }}</p>
-                    <p><a :href="article.link" target="_blank">Read more</a></p>
-                </div>
+            <div class="m-5 bg-white shadow-lg rounded-lg overflow-hidden" v-for="article in $page.frontmatter.articles">
+                <div class="sm:flex sm:items-center px-6 py-4">
+                    <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" :src="'../images/'+article.image" :alt="article.title">
+                    <div class="text-center sm:text-left sm:flex-grow">
+                    <div class="mb-4">
+                        <p class="text-xl leading-tight">{{article.title}}</p>
+                        <p class="text-sm leading-tight text-grey-dark">{{article.date}} - {{ article.blurb }}</p>
+                    </div>
+                    <div>
+                        <button @click="goToArticle(article.link)" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Read More</button>
+                    </div>
+                </div>        
             </div>
+      
     </div>
+</div>
 </template>
 
 <script>
 export default {
-    name: 'BlogLayout'
+    name: 'BlogLayout',
+    methods: {
+        goToArticle(link){
+            window.open(link,'_blank');
+        }
+    }
 }
 </script>
