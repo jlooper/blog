4480a9a7951fa1c5be57c182e1f884dd5b05b032
jlooper <jen.looper@gmail.com>
2018-12-28 11:25:47 -0500

    bumping date and shrinking tailwind file

--- .vuepress/theme/components/Footer.vue
+++ .vuepress/theme/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
  <footer class="footer rounded-b sticky pin-bottom p-6">
     <div class="text-white text-center">
-      &copy 2018 Ladeez First Media
+      &copy 2019 Ladeez First Media
     </div>
 </footer>
 </template>
