4d43e7149f15c0e0e8fff21cfc68a51a241d41d1
Jen Looper <jen.looper@gmail.com>
2019-03-02 13:00:33 -0500

    adding a favicon, layout fix

--- .vuepress/theme/layouts/BasicLayout.vue
+++ .vuepress/theme/layouts/BasicLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="blog-body sm:p-2 sm:m-2 bg-white rounded lg:p-5 lg:m-5 text-blue-darkest">
+    <div class="blog-body  bg-white rounded lg:p-10 lg:m-10 p-2 m-2 text-blue-darkest">
         <h1 class="pb-5 text-blue-darkest">{{$page.frontmatter.title}}</h1>
             <Content/>
     </div>
