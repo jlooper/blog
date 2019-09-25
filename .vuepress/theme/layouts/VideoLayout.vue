4d22e5a1a1b4234d044182cc85279da3bc8d243f
Jen Looper <jen.looper@gmail.com>
2019-09-25 15:46:11 -0400

    Update VideoLayout.vue

--- .vuepress/theme/layouts/VideoLayout.vue
+++ .vuepress/theme/layouts/VideoLayout.vue
@@ -3,10 +3,20 @@
     <h1 class="text-3xl pb-5 pt-5 ml-5 text-gray-800">{{$page.frontmatter.title}}</h1>
     <div class="pl-5 text-gray-700">
       <p>Here are a few of my talks from 2018 - 2019. Click the link below for more.</p>
-      <div v-for="video in videos" :key="video.id" class="m-10 text-center">
+      <div v-for="video in ytvideos" :key="video.id" class="m-10 text-center">
         <iframe
           class="video"
           :src="'https://www.youtube.com/embed/'+video+''"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <div v-for="video in othervideos" :key="video.id" class="m-10 text-center">
+        <iframe
+          class="video"
+          :src="video"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
@@ -32,14 +42,16 @@ export default {
   name: "VideoLayout",
   data() {
     return {
-      videos: [
+      ytvideos: [
+        "4Zhjy3Mdtbc",
         "SgBDEydZb4A",
         "wCU-kDlXY58",
         "ZG7GtgfUS6g",
         "1Kqtg1Mw2wc",
         "H4Rnh01ywJk",
         "dJTRjBHFLFU"
-      ]
+      ],
+      othervideos: ["https://halfstackconf.streameventlive.com/embed/53"]
     };
   }
 };
