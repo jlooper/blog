b0b53f06588ab63c40961644ff89b668ea189411
Jen Looper <jen.looper@gmail.com>
2019-12-01 16:35:15 -0500

    button label was off

--- docs/.vuepress/theme/layouts/ContactLayout.vue
+++ docs/.vuepress/theme/layouts/ContactLayout.vue
@@ -1,63 +1,107 @@
 <template>
   <div class="m-10">
     <h1>Contact Me</h1>
-   
-      <div class="text-xs-center" v-if="submitted">
-        <p>Thank you for your interest, I will get back to you as soon as I can.</p>
+
+    <div class="text-xs-center" v-if="submitted">
+      <p>Thank you for your email, I will get back to you as soon as I can.</p>
+    </div>
+
+    <form
+      class="w-full max-w-lg"
+      @submit.prevent="handleSubmit"
+      netlify
+      name="contact-me"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      v-else
+      v-model="valid"
+    >
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+          >First Name</label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+            :rules="rules"
+            v-model="firstName"
+            name="firstName"
+            required
+          />
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >Last Name</label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+            :rules="rules"
+            v-model="lastName"
+            name="lastName"
+            required
+          />
+        </div>
       </div>
-      
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-email"
+          >Email</label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-password"
+            type="email"
+            placeholder="my@email.com"
+            :rules="rules"
+            v-model="email"
+            name="email"
+            required
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-2">
+        <div class="w-full px-3 mb-6">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-message"
+          >Message</label>
+          <textarea
+            cols="50"
+            rows="10"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-message"
+            type="text"
+            placeholder="My message to you"
+            :rules="rules"
+            v-model="message"
+            name="message"
+            required
+          ></textarea>
+        </div>
+      </div>
 
-      <form class="w-full max-w-lg" 
-          @submit.prevent="handleSubmit"
-          netlify
-          name="contact-me"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          v-else
-          v-model="valid"
-          >
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-              First Name
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" :rules="rules" v-model="firstName" name="firstName" required>
-          </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-              Last Name
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" :rules="rules" v-model="lastName" name="lastName" required>
-          </div>
+      <div class="md:flex md:items-center">
+        <div class="md:w-1/3"></div>
+        <div class="md:w-2/3">
+          <button
+            type="submit"
+            :disabled="!valid"
+            class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+          >Submit</button>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
-              Email
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="my@email.com" :rules="rules" v-model="email" name="email" required>
-          </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-2">
-          <div class="w-full px-3 mb-6">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-message">
-              Message
-            </label>
-            <textarea cols="50" rows="10"  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-message" type="text" placeholder="My message to you" :rules="rules" v-model="message" name="message" required></textarea>
-          </div>
-        </div>
-
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3"></div>
-          <div class="md:w-2/3">
-            <button type="submit" :disabled="!valid" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
-              Sign Up
-            </button>
-          </div>
-        </div>
-      </form>
-   </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -102,9 +146,7 @@ export default {
         })
       })
         .then(() => {
-          alert(
-            "Thank you for your email! I will reply as soon as I can."
-          );
+          alert("Thank you for your email! I will reply as soon as I can.");
         })
         .catch(() => {
           alert("Sorry, there seems to have been an error.");
