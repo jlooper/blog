6fcb111a955312aa1bc7c12d6c94b6ab6c7c37dc
Jen Looper <jen.looper@gmail.com>
2020-04-19 13:35:18 -0400

    RWD and missing links

--- docs/.vuepress/theme/layouts/HomeLayout.vue
+++ docs/.vuepress/theme/layouts/HomeLayout.vue
@@ -1,38 +1,26 @@
 <template>
   <div>
     <div class="mt-0 lg:mt-2 md:mt-0 sm:mt-0">
-      <div class="flex -mx-2">
+      <div class="flex">
         <div class="w-full lg:w-1/2 px-2">
-          <div class="h-56 float-right">
-            <div class="cursor-pointer" @click="goToSite('https://yomamaisa.dev')">
-              <div class>
-                <Img src="./images/jen-bg.png" />
-              </div>
+          <div class="float-right">
+            <div class="pt-5">
+              <Img src="./images/jen-bg.png" />
             </div>
           </div>
         </div>
         <div class="w-full lg:w-1/2 px-2">
-          <div class="h-56">
-            <div class="cursor-pointer" @click="goToSite('https://www.quicknoms.com')">
-              <div class="inline-block align-top">
-                <h2
-                  class="text-3xl lg:text-3xl md:text-2xl sm:xl p-2 text-white text-left"
-                >Hi, I'm Jen</h2>
-                <p
-                  class="text-2xl leading-snug lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
-                >Developer Advocate</p>
-                <p
-                  class="text-2xl leading-snug lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
-                >Speaker</p>
-                <p
-                  class="text-2xl leading-snug lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
-                >Author</p>
-                <p
-                  class="text-2xl leading-snug lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
-                >Builder of Interesting Things</p>
-              </div>
-            </div>
-          </div>
+          <h1 class="pt-5 text-white text-left">Hi, I'm Jen</h1>
+          <p
+            class="text-2xl lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
+          >Developer Advocate</p>
+          <p
+            class="text-2xl lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
+          >Speaker and Author</p>
+
+          <p
+            class="text-2xl lg:text-2xl md:text-xl sm:text-base p-2 text-white text-left"
+          >Builder of Interesting Things</p>
         </div>
       </div>
     </div>
